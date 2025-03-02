f503dad4583925a960013f42f09062ad7d87b280
Stephen A <91034191+StephenA1312@users.noreply.github.com>
2025-03-02 04:31:31 -0800

    Implement EmailService (#5)
    * implemented sendviaemail
    
    * env
    
    * env
    
    * Stop tracking .env
    
    * remove env
    
    * Stop tracking .env
    
    ---------
    
    Co-authored-by: Stephen Ali <stephenali@Stephens-MacBook-Air.local>

---
+++ pingStream/src/main/java/com/pingStream/pingStream/controller/EmailController.java
@@ -0,0 +1,37 @@
+package com.pingStream.pingStream.controller;
+
+import com.pingStream.pingStream.model.Notification.EmailRequest;
+import com.pingStream.pingStream.service.EmailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/email") // Base URL for email-related endpoints
+public class EmailController {
+    private final EmailService emailService;
+    @Autowired
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
+        boolean success = emailService.send(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getBody());
+
+        if (success) {
+            return ResponseEntity.ok("Email sent successfully!");
+        } else {
+            return ResponseEntity.status(500).body("Failed to send email.");
+        }
+    }
+
+    @GetMapping("/works")
+    public String getWorks(){
+        log.info("Testing here");
+        return "API works Stephen!!";
+    }
+
+}

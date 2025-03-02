f57f58c161c921a68ec04f3870fefff4dc9fa416
Stephen Ali <alistephen64@gmail.com>
2025-03-02 01:34:28 -0800

    changes

---
+++ pingStream/src/main/java/com/pingStream/pingStream/service/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.pingStream.pingStream.service;
+
+import com.pingStream.pingStream.model.Notification.Notification;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationRepository {
+    public void save(Notification notification) {
+    }
+}
